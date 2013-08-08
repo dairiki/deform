cee3f9292a6d1b36e357d5ad30302a4834af350f
Jeff Dairiki <dairiki@dairiki.org>
2013-08-08 12:50:23 -0700

    Avoid spontaneous DeprecationWarning when importing deform.
    See https://github.com/Pylons/deform/issues/175

--- deform/__init__.py
+++ deform/__init__.py
@@ -1,3 +1,5 @@
+from zope.deprecation import __show__, deprecated
+
 from .field import Field # API
 
 from .form import Form # API
@@ -6,8 +8,19 @@ from .form import Button # API
 from .exception import ValidationFailure # API
 from .exception import TemplateError # API
 
+__show__.off()
 from .schema import Set # API
+__show__.on()
 from .schema import FileData # API
 
 from .template import ZPTRendererFactory # API
 from .template import default_renderer # API
+
+deprecated(
+    'Set',
+    'deform.Set is deprecated as of the release of Colander > 1.0a1, which '
+    'includes its own Set class.  colander.Set is not exactly a drop-in '
+    'replacement (it has no allow_empty constructor argument), but you should '
+    'switch to it as soon as possible.  The deform.Set class will be removed '
+    'in Deform 1.1+'
+    )
--- deform/tests/test_api.py
+++ deform/tests/test_api.py
@@ -1,7 +1,10 @@
 import unittest
+import warnings
+from zope.deprecation import __show__
 
 class TestAPI(unittest.TestCase):
     def test_it(self):
+        __show__.off()
         # none of these imports should fail
         from deform import Form
         from deform import Button
@@ -17,8 +20,11 @@ class TestAPI(unittest.TestCase):
         from deform import default_renderer
 
         from deform import widget
-        
-        
-        
-        
-        
+        __show__.on()
+
+    def test_Set_deprecation(self):
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("error")
+            def import_Set():
+                from deform import Set
+            self.assertRaises(DeprecationWarning, import_Set)
